6d46ec2768c6a54879c40ef8a67bf4aa521293a0
Matheus Richard <matheusrichardt@gmail.com>
2024-08-26 09:56:49 -0300

    Look for envs globally

--- lib/matheus.rb
+++ lib/matheus.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-require "dotenv/load"
+require "dotenv"
 require "active_support"
 require "active_support/core_ext"
 require "zeitwerk"
 
+Dotenv.load("~/.env")
 Zeitwerk::Loader.for_gem.setup
 
 module Matheus
