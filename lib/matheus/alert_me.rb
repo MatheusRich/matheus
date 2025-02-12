c54d5150086a9efc82e8e05ea5f664cb5d1a33ba
Matheus Richard <matheusrichardt@gmail.com>
2025-02-12 11:10:02 -0300

    alert-me without args should work

--- lib/matheus/alert_me.rb
+++ lib/matheus/alert_me.rb
@@ -8,7 +8,7 @@ module Matheus
     #    $ alert-me "sleep 1 && echo 'Done!'"
     #    Runs the command and plays a sound based on its success or failure after it finishes.
     def call(*args)
-      if system(args.join(" "))
+      if system(args.join(" ").presence || "true")
         system("afplay /System/Library/Sounds/Glass.aiff")
       else
         system("afplay /System/Library/Sounds/Sosumi.aiff")
